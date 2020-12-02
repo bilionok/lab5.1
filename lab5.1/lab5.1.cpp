277bc961c6eec2c2df0abec0013f555cdc54b570
bilionok <photoshoper16@gmail.com>
2020-12-03 00:00:15 +0200

    lab5.1

---
+++ lab5.1/lab5.1.cpp
@@ -0,0 +1,21 @@
+﻿// Lab 5_1
+// Біліонок Ярослав
+// Варіант 1
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+double k(const double x, const double y); // прототип
+int main()
+{
+	double p, q;
+	cout << "a = "; cin >> p;
+	cout << "b = "; cin >> q;
+	double c = (k(1 + p * q, q * q) + pow(k(p, p * p), 2)) / (1 + k(p * q + q * q, p));
+	cout << "c = " << c << endl;
+	return 0;
+}
+double k(const double x, const double y) // визначення
+{
+	return x / 1 + sin(y) * sin(y) + y / 1 + x * x;
+}
